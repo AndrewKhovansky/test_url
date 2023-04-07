e3059d520042724ac78ed15aeb806d182891f085
AndrewKhovansky <andrew_2006@mail.ru>
2023-04-07 23:24:05 +0300

    Comments, arguments check

--- test.cpp
+++ test.cpp
@@ -9,27 +9,20 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	if(argc < 2)
+	if(argc < 3) //Check for arguments
 	{
-		cout << "Nothing to parse\r\n";
+		cout << "Not enough arguments\r\n";
 		return 1;
 	}
 	
-	
-	if(argc < 3)
-	{
-		cout << "No element index given\r\n";
-		return 1;
-	}
-	
-	int index = atoi(argv[2]);
+	int index = atoi(argv[2]); //Index of element
 	
 	
 	
-	URLParser parser(argv[1]);
+	URLParser parser(argv[1]); //Construct parser with an input string
 	
 	
-	enum Method method = parser.get_method();
+	enum Method method = parser.get_method(); //Get method 
 	
 	if(method == MethodPUT) cout << "Method: PUT\r\n";
 	if(method == MethodGET) cout << "Method: GET\r\n";
@@ -37,7 +30,7 @@ int main(int argc, char *argv[])
 	if(method == MethodDELETE) cout << "Method: DELETE\r\n";
 	if(method == MethodNONE) cout << "Method: NONE\r\n";
 	
-	cout << parser.get_element(index);
+	cout << parser.get_element(index) << "\r\n";
 	
 	return 1;
 }
--- urlparser.cpp
+++ urlparser.cpp
@@ -9,50 +9,46 @@
 #include <string>
 #include <vector>
 #include <iterator>
-#include <sstream>      // std::istringstream
+#include <sstream>   
 #include "urlparser.h"
 using namespace std;
 
 
 
-URLParser::URLParser(const char* instring)
+URLParser::URLParser(const char* instring) //Constructor
 {
-	input = std::string(instring);
-	
+	input = std::string(instring); //Init an input string (convert from const char* to std::string)
 }
 
 
-enum Method URLParser::get_method(void)
+enum Method URLParser::get_method(void) //URL method parser
 {
-	std::string token = input.substr(0, input.find(" "));
+	std::string token = input.substr(0, input.find(" ")); //Get a substring from begin to a ' ' delimiter
 	
 	if (token == "GET") return MethodGET;
 	else if (token == "POST") return MethodPOST;
 	else if (token == "PUT") return MethodPUT;
 	else if (token == "DELETE") return MethodDELETE;
 	
-	return MethodNONE;
+	return MethodNONE; //No method
 }
 
 
-std::string URLParser::get_element(int index)
+std::string URLParser::get_element(int index) //Get an element 
 {
-    std::vector<std::string>  strarray;
-    std::stringstream streamData(input);
+    std::vector<std::string>  strarray; //Init an output vector (array of strings)
+    std::stringstream streamData(input); //Create stringstream
     std::string val;
 
-    while (std::getline(streamData, val, '/')) {
-        strarray.push_back(val);
+    while (std::getline(streamData, val, '/')) //Get lines from stringstream to a val variable. '/' is a delimiter 
+	{
+        strarray.push_back(val); //Append value to the end of array
     }
 				
-    if(strarray.size() < (index + 4))
+    if(strarray.size() < (index + 4)) //Check bounds. Skip three '/' delimiters (two after http: and one after domain name)
 	{
 		return "";
 	}
-
-
-				
-	return strarray[index + 3];
-								 
-								 
+		
+	return strarray[index + 3]; //Skip three '/' delimiters
 }
