951ca373279e0a84e83b6f4b4d8da30ed90c828d
AndrewKhovansky <andrew_2006@mail.ru>
2023-04-07 20:30:34 +0300

    Project created

---
+++ testurl.cpp
@@ -0,0 +1,30 @@
+/*
+ * testurl.cpp
+ *
+ *  Created on: 7 апр. 2023 г.
+ *      Author: khova
+ */
+
+enum Method
+{
+	MethodPOST,
+	MethodGET,
+	MethodDELETE,
+	MethodPUT,
+};
+
+
+class URLParser
+{
+	URLParser(const char* input)
+	{
+
+	}
+};
+
+
+int main()
+{
+
+
+}
